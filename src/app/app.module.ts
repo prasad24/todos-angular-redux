a64906524cd6dba1e6c3190fec9e8504e4c2b0dd
Prasad Subramanain <prasad2@hotmail.com>
2018-07-20 10:05:57 -0400

    Added add, list and overview components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { OverviewComponent } from './components/overview/overview.component';
+import { AddComponent } from './components/add/add.component';
+import { ListComponent } from './components/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OverviewComponent,
+    AddComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule
